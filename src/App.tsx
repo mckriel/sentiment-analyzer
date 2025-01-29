a7307ea3eb0992a5d1b54a12778eb4bd37519386
mckriel <mckriel@gmail.com>
2025-01-29 11:31:28 +0200

    app.tsx and .env.template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import { React, useState } from 'react';
+import { ComprehendClient, DetectSentimentCommand } from '@aws-sdk/client-comprehend';
 import './App.css';
 
+// Create a sentiment interface
+interface SentimentResult {
+	text: string,
+  	score: {
+		Postive: number,
+		Negative: number,
+		Neutral: number,
+		Mixed: number,
+	};
+}
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [inputText, setInputText] = useState('');
+	const [results, setResults] = useState<SentimentResult[]>([]);
+
+	// AWS configuration
+	const client = new ComprehendClient({ 
+		region: process.env.REACT_APP_AWS_REGION,
+		credentials: {
+			accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID!,
+			secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY!,
+		} 
+	});
+
+	const handleSubmit = async (event: React.FormEvent) => {
+		event.preventDefault();
+	};
+
+	return (
+		<div className="App">
+			<form onSubmit={handleSubmit}>
+				<input 
+					type="text" 
+					value={inputText}
+					onChange={(e) => setInputText(e.target.value)}
+					placeholder="Let's analyze some text!"
+				/>
+				<button type="submit">
+					Get Sentiment
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
